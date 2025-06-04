d86e05b89ffaf4e50d3ff1499bb76761af383f0a
majvax <majvax.contact@gmail.com>
2025-06-04 16:29:52 +0200

    tp_ex4 done

--- tp_ex4/client.sh
+++ tp_ex4/client.sh
@@ -2,13 +2,32 @@
 
 SERVER=localhost
 PORT=12344
+KEY="QWERTYUIOPASDFGHJKLZXCVBNM"
+
+send() {
+  echo "$1" | tr 'A-Z' "$KEY" | tr 'a-z' "$(echo "$KEY" | tr 'A-Z' 'a-z')"
+}
+
+decode() {
+  alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  echo "$1" | tr "$KEY" "$alphabet" | tr "$(echo "$KEY" | tr 'A-Z' 'a-z')" 'a-z'
+}
 
 function interpret() {
-  while true; do
-    read -rp "> " cmd
-    echo "$cmd"
+  read -r password
+  send "$password"
+
+  while read -r cmd; do
+    send "$cmd"
   done
 }
 
-interpret | nc "$SERVER" "$PORT"
+function recv() {
+  while read -r response; do
+    decode "$response"
+  done
+}
+
+interpret | nc "$SERVER" "$PORT" | recv
+
 echo "disconnected, au revoir!"
