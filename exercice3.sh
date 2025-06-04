52db6435c3dca2b0dc6b640a10d046f00e257d57
majvax <majvax.contact@gmail.com>
2025-06-04 20:02:24 +0200

    fixed few problem including filename

--- exercice3.sh
+++ exercice3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2181,SC2094 
+# shellcheck disable=SC2181,SC2094
 
 IP=localhost
 PORT=12345
@@ -8,35 +8,30 @@ rm ./fifo
 mkfifo ./fifo
 is_open=true
 
-
-while read -r line ; do
-  eval "$line"
-done < configuration.conf
-
+source configuration.conf
 
 # check if password exists
 # see https://stackoverflow.com/a/13864829/214577 for reference
-if [ -z ${password+x} ] ; then
+if [ -z ${password+x} ]; then
   echo "missing 'password' key in configuration.conf"
   exit 1
 fi
 
-
-function interpret () {
-  echo "Veuiller entrer un mot de passe." 
+function interpret() {
+  echo "Veuiller entrer un mot de passe."
   read -r pswd
-  if [ "$password" != "$pswd" ] ; then
+  if [ "$password" != "$pswd" ]; then
     echo "Mot de passe faux."
     return 0
   fi
 
   echo "Mot de passe bon!"
 
-  while read -r line ; do
+  while read -r line; do
     # Allow the client to close the server
     # This will ensure that the socket close properly
     # So the port is reusable
-    if [ "$line" == "/close" ] ; then
+    if [ "$line" == "/close" ]; then
       echo "bye bye!"
       return 1
     else
@@ -49,14 +44,12 @@ function interpret () {
   return 0
 }
 
-
-while [ "$is_open" = true ]
-do
-  nc -l -s "$IP" -p "$PORT" < ./fifo | ( interpret ) > ./fifo
+while [ "$is_open" = true ]; do
+  nc -l -s "$IP" -p "$PORT" <./fifo | (interpret) >./fifo
 
   # Use the error code of interpret cause
   # the function is running in a subshell
-  if [ $? -ne 0 ] ; then
+  if [ $? -ne 0 ]; then
     is_open=false
   fi
 done
